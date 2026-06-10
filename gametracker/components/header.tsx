c87050d602d9c32ae7ece31f063164d1b07ffae2
daeweeee <hofhansl.david@gmail.com>
2026-06-10 11:50:45 +0200

    problémy s dětmi

--- gametracker/components/header.tsx
+++ gametracker/components/header.tsx
@@ -1,7 +1,13 @@
-export default function Header({ children } ) {
+import React from "react";
+
+type HeaderProps = {
+    children?: React.ReactNode;
+};
+
+export default function Header({ children }: HeaderProps) {
     return (
         <div className="flex mb-5 w-full">
-            { children }
+            {children}
         </div>
     );
 }
--- gametracker/components/stats.tsx
+++ gametracker/components/stats.tsx
@@ -1,4 +1,10 @@
-export default function Stats({ children } ) {
+import { ReactNode } from "react";
+
+type StatsProps = {
+    children: ReactNode;
+};
+
+export default function Stats({ children }: StatsProps) {
     return (
         <div className="flex gap-5 mt-10 mb-10 w-full">
             { children }
